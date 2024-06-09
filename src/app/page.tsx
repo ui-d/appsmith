990d38dcfedbfc518c6c418ee4b96cdacef9129a
ui-d <dawiddeveloper@gmail.com>
2024-06-09 15:55:42 +0200

    project scaffold

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+// TODO: Remove this entire file and start your own project from scratch.
+// Happy coding!
+
+import Link from "next/link";
+import { GithubIcon } from "lucide-react";
+
+import { siteConfig } from "@/config";
+import { cn } from "@/lib/utils";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { buttonVariants } from "@/components/ui/button";
+
+export default function IndexPage() {
+  return (
+    <main className="container grid max-w-prose items-center gap-6 space-y-4 pb-8 pt-6 text-balance md:py-10">
+
+     test
+    </main>
+  );
+}
